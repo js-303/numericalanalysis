b57e6bdba1f3723e9abc86922bb071367c0c1283
js-303 <118151418+js-303@users.noreply.github.com>
2024-07-02 06:16:54 +0000

    good root to compare estimates

--- modifiednewton.py
+++ modifiednewton.py
@@ -1,8 +1,9 @@
 import numpy as np 
+from scipy.optimize import root
 from math import e
 
-f = lambda x : 27*x**3+54*x**2+36*x+8
-f_prime = lambda x: 81*x**2+108*x+36
+f = lambda x : 2*e**(x-1)-x**2-1
+f_prime = lambda x: 2*e**(x-1)-2*x
 x_0 = float(input("input initial guess(float): "))
 p = int(input("accurate to how many decimal places(int): "))
 tol = 0.5 * 10**(-p)
@@ -17,10 +18,11 @@ def modifiednewton(f, df, x_0, tol):
 estimate = modifiednewton(f, f_prime, x_0, tol)
 print("estimate =", np.round(estimate, p))
 
-coef = [27, 54, 36, 8]
-r = np.roots(coef)
-print("root: ", np.round(r[0], p))
-f_error = abs(np.round((r[0] - estimate), p))
+
+r = root(f, x_0)
+print("root: ", np.round(r.x, p))
+f_error = abs(np.round((r.x - estimate), p))
 print("forward error: ", f_error)
 b_error = abs(np.round(f(estimate), p))
 print("backward error: ", b_error)
+
