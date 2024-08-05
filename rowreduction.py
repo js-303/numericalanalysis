a339b3a0c0d8710bc15f40b4ead440b72db525ef
js-303 <118151418+js-303@users.noreply.github.com>
2024-08-05 21:41:53 +0000

    8/5/24

--- rowreduction.py
+++ rowreduction.py
@@ -2,10 +2,10 @@ import numpy as np
 import numpy.linalg as la
 from numpy import array, inf, zeros_like, empty
 
-A = array([[1,2,3],
-           [4,5,6],
-           [7,8,9]], dtype=float)
-b = array([3,2,1], dtype=float)
+A = array([[2,-2,-1],
+           [4,1,-2],
+           [-2,1,-1]], dtype=float)
+b = array([-2,1,-3], dtype=float)
 
 
 def rowreduce(A, b):
@@ -14,16 +14,19 @@ def rowreduce(A, b):
     n = len(b)
 
     L = zeros_like(A)
-    for k in range(n-1):
+    for k in range(n):
         for i in range(k+1, n):
             L[i,k] = U[i,k]/U[k,k]
+            print(L[i,k])
             for j in range(k+1, n):
+                print(j)
                 U[i,j] -= L[i,k]*U[k,j]
                 U[i,k] = 0
-                c[i] -= L[i,k] *c[k]
+                c[i] -= L[i,k]*c[k]
+                print(U)
     return (U,c)
 
-
+"""
 def backsub(A, b):
     U = A.copy()
     c = b.copy()
@@ -33,7 +36,6 @@ def backsub(A, b):
     for i in range(n-2, -1, -1):
         x[i] = (c[i] - sum(U[i,i+1:] * x[i+1:])) / U[i,i]
     return x
-
-print(rowreduce(A, b))
-print(backsub(A, b))
-
+"""
+print(rowreduce(A,b))
+print(la.solve(A,b))
