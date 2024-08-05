b5dd20db7d0c90e880a5545c1eb3bc4e31f0701d
js-303 <118151418+js-303@users.noreply.github.com>
2024-08-05 05:07:46 +0000

    8/4/24

--- modifiednewton.py
+++ modifiednewton.py
@@ -1,9 +1,11 @@
 import numpy as np 
 from scipy.optimize import root
-from math import e, log
+import matplotlib.pyplot as plt
+from numpy import linspace
+from math import e, log, pi, sin, cos
 
-f = lambda x : log(3-x)+x-2
-f_prime = lambda x: -1*(3-x)**(-1)+1
+f = lambda x : e**((sin(x))**3)+x**6-2*x**4-x**3-1
+f_prime = lambda x: (3*cos(x)*(sin(x))**2)*e**((sin(x))**3)+6*x**5-8*x**3-3*x**2
 x_0 = float(input("input initial guess(float): "))
 p = int(input("accurate to how many decimal places(int): "))
 tol = 0.5 * 10**(-p)
@@ -26,12 +28,19 @@ print("forward error: ", f_error)
 b_error = abs(np.round(f(estimate), p))
 print("backward error: ", b_error)
 
+f_v = np.vectorize(f)
+x = linspace(-pi, pi)
+plt.plot(x, f_v(x), 'b')
+plt.show()
+
+
 
 print("f(r) = ", f(r.x))
 print("f'(r) = ", f_prime(r.x))
 
-f_prime_2 = lambda x: -1*(3-x)**(-2)
+"""
+f_prime_2 = lambda x: (20/3)*pi+4*pi*x_0
 print("f''(r) =  ", f_prime_2(r.x))
-f_prime_3 = lambda x: -2*(3-x)**(-3)
+f_prime_3 = lambda x: 
 print("f'''(r) = ", f_prime_3(r.x))
-
+"""
--- newton.py
+++ newton.py
@@ -1,9 +1,11 @@
 import numpy as np 
+from numpy import linspace
 from scipy.optimize import root
-from math import e, log
+from math import e, log, pi, sin, cos, inf
+from matplotlib import pyplot as plt
 
-f = lambda x : log(3-x)+x-2
-f_prime = lambda x: -1*(3-x)**(-1)+1
+f = lambda x : (e**sin(x)**3)+x**6-2*x**4-x**3-1
+f_prime = lambda x: (3*cos(x)*(sin(x)**2))*(e**(sin(x)**3))+6*x**5-8*x**3-3*x**2
 x_0 = float(input("input initial guess(int): "))
 p = int(input("correct to how many decimal places(int): "))
 tol = 0.5 * 10**(-p)
@@ -26,7 +28,11 @@ print("backward error: ", b_error)
 
 print("f(r) = ", f(r.x))
 print("f'(r) = ", f_prime(r.x))
+
+"""""
 f_prime_2 = lambda x: -1*(3-x)**(-2)
 print("f''(r) =  ", f_prime_2(r.x))
 f_prime_3 = lambda x: -2*(3-x)**(-3)
-print("f'''(r) = ", f_prime_3(r.x))
+print("f'''(r) = ", f_prime_3(r.x)) 
+"""
+
