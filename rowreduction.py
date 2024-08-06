ca425a0857741440298ae50729614d759a8e4655
js-303 <118151418+js-303@users.noreply.github.com>
2024-08-06 16:26:44 +0000

    8/6/24

--- rowreduction.py
+++ rowreduction.py
@@ -2,11 +2,10 @@ import numpy as np
 import numpy.linalg as la
 from numpy import array, inf, zeros_like, empty
 
-A = array([[2,-2,-1],
-           [4,1,-2],
-           [-2,1,-1]], dtype=float)
-b = array([-2,1,-3], dtype=float)
-
+A = array([[1,2,-1],
+           [0,3,1],
+           [2,-1,1]], dtype=float)
+b = array([2,4,2], dtype=float)
 
 def rowreduce(A, b):
     U = A.copy()
@@ -17,25 +16,28 @@ def rowreduce(A, b):
     for k in range(n):
         for i in range(k+1, n):
             L[i,k] = U[i,k]/U[k,k]
-            print(L[i,k])
             for j in range(k+1, n):
-                print(j)
                 U[i,j] -= L[i,k]*U[k,j]
                 U[i,k] = 0
                 c[i] -= L[i,k]*c[k]
-                print(U)
     return (U,c)
 
-"""
-def backsub(A, b):
-    U = A.copy()
-    c = b.copy()
+(U, c) =rowreduce(A,b)
+
+def backsub(U, c):
     n = len(c)
     x = np.zeros(n)
     x[n-1] = c[n-1]/U[n-1,n-1]
     for i in range(n-2, -1, -1):
         x[i] = (c[i] - sum(U[i,i+1:] * x[i+1:])) / U[i,i]
     return x
-"""
-print(rowreduce(A,b))
+
+(U, c) =rowreduce(A,b)
+print(U)
+x = backsub(U,c)
+print(f"x = {x}")
+r = b - A@x
+print(f"/nresidual b - Ax = {r}")
+print(f"max norm {max(abs(r)):.3}")
 print(la.solve(A,b))
+
