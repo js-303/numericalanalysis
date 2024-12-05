92c00f0f5546ebb5d2261d29adf41c7f7218e177
js-303 <118151418+js-303@users.noreply.github.com>
2024-12-05 15:56:10 -0700

    Add files via upload

--- hw10.py
+++ hw10.py
@@ -1,43 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-i=0
 monthly_kva = {}
 yearly_kva = {}
+month = []
+b = []
 
-with open("C:\\Users\jamil\Documents\MATH4650\homework\windmill.txt", "r") as file:
+with open("C:\\Users\Jamil Santos\Documents\math4650\homework\windmill.txt", "r") as file:
     lines = [int(line.rstrip()) for line in file]
 file.close()
 for i in range(len(lines)):
     monthly_kva[i+1] = lines[i]
-def yearly(dict):
-    values = list(dict.values())
-    result = []
-    for i in range(0, len(values), 12):
-        result.append(sum(values[i:i+12]))
-    return result
-for i in range(len(yearly(monthly_kva))):
-    yearly_kva[i+1] = yearly(monthly_kva)[i]
+    month.append((i+1)/12)
+    b.append(lines[i])
 
-
-
-t = np.array([0, 1, 2, 3, 4, 5])
-A = np.vstack([np.ones(len(t)),np.cos(2*np.pi*t),np.sin(2*np.pi*t),np.cos(4*np.pi*t)]).T
-b = np.array([0,2726,2772,2912,2702,2682])
-print(A)
+t = np.array(month)
+A = np.vstack([np.ones(len(t)),np.cos((2*np.pi)*t),np.sin((2*np.pi)*t),np.cos((4*np.pi)*t)]).T
+b = np.array(b)
+print(t)
+print(b)
 
 def least_squares_qr(A, b):
   m, n = A.shape
   Q = np.zeros((m, n))
   R = np.zeros((n, n))
 
-  for j in range(n): # working with column j
+  for j in range(n): 
     v = A[:, j]
-    for i in range(j): # all columns i before j are already orthonormal
-      R[i, j] = np.dot(Q[:, i], v) # project column j on column i
-      v = v - R[i, j] * Q[:, i]    # find part of column j orthogonal to column i
-
-    R[j, j] = np.linalg.norm(v) #normalize column j
+    for i in range(j): 
+      R[i, j] = np.dot(Q[:, i], v) 
+      v = v - R[i, j] * Q[:, i]    
+    R[j, j] = np.linalg.norm(v)
     Q[:, j] = v / R[j, j]
   Qb = np.dot(Q.T, b)
   x = np.zeros(n)
@@ -51,7 +44,17 @@ def least_squares_qr(A, b):
 QR = least_squares_qr(A,b)
 print(QR)
 
-f = lambda t: QR[3]+QR[2]*np.cos(2*np.pi*t)+QR[1]*np.sin(2*np.pi*t)+QR[0]*np.cos(4*np.pi*t)
+f = lambda t: QR[0]+QR[1]*np.cos(2*np.pi*t)+QR[2]*np.sin(2*np.pi*t)+QR[3]*np.cos(4*np.pi*t)
 tt = np.linspace(0, 5, 1000)
-plt.plot(tt, f(tt))
+ax = plt.axes()
+plt.plot(tt, f(tt), label='periodic fit')
+for i in range(len(b)):
+   plt.plot(t[i], b[i], "o")
+plt.title("Periodic fit of monthly MWh from JAN 2005 to DEC 2009")
+plt.ylabel("MWh per month")
+plt.xlabel("year t")
+plt.legend()
 plt.show()
+
+
+
